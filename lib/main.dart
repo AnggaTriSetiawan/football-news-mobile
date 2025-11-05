833f42e0ed7050431b3e9096d80cceec534eb220
AnggaTriSetiawan <anggatrisetiawan81@gmail.com>
2025-11-05 16:07:17 +0700

    tutorial 7 selesai

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Football News',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
         .copyWith(secondary: Colors.blueAccent[400]),
